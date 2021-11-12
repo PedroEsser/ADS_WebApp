359bbea5a2539e3c64cbbe1d1c05f5d837a131f4
PedroEsser <pedro.esser@hotmail.com>
2021-11-12 00:34:33 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
+const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000
 let docker
+let last_client
 
 app.listen(port, () => {
   console.log('ADS App listening on port ' + port)
 })
 
-app.get('/test', (req, res) => {
-  //docker_client_uri = req.params.uri
+app.get('/docker_connect', (req, res) => {
+  //if (/* is really our docker)*/)
+  console.log("Docker connect")
   docker = res
 })
 
-app.get('/:data', (req, res) => {
-  if (docker){
-    docker.send("<a style =\" font-size:150px \" >" + req.params.data + "</a>")
-    docker = null
+router.post("/docker_post",(request, response) => {
+  console.log("Docker post:")
+  if(last_client){
+    last_client.send(request.body.data)
+    last_client = null
   }
-    
-  res.send("")
-})
+});
 
-app.get('', (req, res) => {
-  res.send("")
+app.use("/", router);
+
+app.all('*', (req, res) => {
+  console.log("All :" + req.params[0])
+  if (docker){
+    docker.send(req.params[0])
+    docker = null
+    last_client = res
+  }else{
+    res.send("Docker not connected yet!")
+  }
 })
 
