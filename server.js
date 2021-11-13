ffbb3bbf3d15e0f1d756536ad8b59a617ccc57d2
PedroEsser <pedro.esser@hotmail.com>
2021-11-13 00:09:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000
 let docker
-let last_client
+let client_id = 0
+let clients = new Map()
 
 app.listen(port, () => {
   console.log('ADS App listening on port ' + port)
@@ -21,30 +22,29 @@ app.get('/docker_connect', (req, res) => {
 
 router.post("/docker_post",(request, response) => {
   console.log("Docker post:")
-  if(last_client){
+  let id = parseInt(request.body.id)
+  clients.get(id).send(request.body.data)
+  clients.delete(id)
+  /*if(last_client){
     last_client.send(request.body.data)
     last_client = null
-  }
+  }*/
 });
 
 app.use("/", router);
 
-app.get('', (req, res) => {
-  if (docker){
-    docker.send(req.params[0])
-    docker = null
-    last_client = res
-  }else{
-    res.send("Docker not connected yet!")
-  }
-})
+app.get('', handle_client)
 
-app.get('/*', (req, res) => {
+app.get('/*', handle_client)
+
+function handle_client(req, res){
   if (docker){
-    docker.send(req.params[0])
+    client_id += 1
+    let message = req.params[0] || " "
+    docker.send(client_id + "/" + message)
     docker = null
-    last_client = res
+    clients.set(client_id, res)
   }else{
     res.send("Docker not connected yet!")
   }
-})
+}
