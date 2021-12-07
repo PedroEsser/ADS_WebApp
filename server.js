00e57bcc46a6f59eb383841f95af6b6e76b4b0eb
PedroEsser <pedro.esser@hotmail.com>
2021-12-07 12:40:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const port = process.env.PORT || 5000
 let docker_clients = []
 let client_id = 0
 let clients = new Map()
+let local_uri = "http://localhost:8080"
+let remote_uri = "https://ads-tunnel.herokuapp.com";//"http://localhost:5000";//
 
 const server = app.listen(port, () => {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('ADS app listening at http://%s:%s', host, port)
+  console.log('ADS app listening at %s', remote_uri)
 })
 
 router.get('/docker_hello', (req, res) => {
+  baseUrl = req.baseUrl
   docker_clients.unshift(res)
   res.setTimeout(20000, () => {
     res.send("Timed out")
@@ -32,13 +33,14 @@ router.post("/docker_post", (req, res) => {
   for(property in headers)
     headers[property] = headers[property][0]
 
-  client_res.set(headers)
-
-  if("Location" in headers) 
+  if("Location" in headers) {
+    headers["Location"] = headers["Location"].replace(local_uri, remote_uri)
+    client_res.set(headers)
     client_res.redirect(headers["Location"])
-  else
+  }else{
+    client_res.set(headers)
     client_res.send(req.body.data)
-  
+  }
   console.log(headers)
   
   clients.delete(id)
@@ -50,7 +52,7 @@ router.get('/debug', (req, res) => {
   var port = server.address().port
   let debug = "<h1>Active docker connections: " + docker_clients.length + "</h1><br>"
   debug += "<h2>Current client id: " + client_id + "</h2><br>"
-  debug += "<h2> Server at http://" + app.mountpath + "</h2><br>"
+  debug += "<h2> Server at http://" + baseUrl + "</h2><br>"
   res.send(debug)
 })
 
