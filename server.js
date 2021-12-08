c48db59ac2e7b6ba272fc9dab7997e53764b77e0
PedroEsser <pedro.esser@hotmail.com>
2021-12-08 00:12:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let docker_clients = []
 let client_id = 0
 let clients = new Map()
 let local_uri = "http://localhost:8080"
-let remote_uri = "https://fenix.iscte-iul.pt";//"http://localhost:5000";//"https://ads-tunnel.herokuapp.com";//
+let remote_uri = "https://e-learning.iscte-iul.pt";//"http://localhost:5000";//"https://ads-tunnel.herokuapp.com";//
 
 app.listen(port, () => {
   console.log('ADS app listening at %s', remote_uri)
